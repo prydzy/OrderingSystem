57264b341e27146afcf2e0a61fb17beff1a6cdb1
unknown <clark7491@live.com>
2018-06-04 01:29:14 +0100

    added more tests for dispatching

--- OrderingSystem/src/tests/OrderTesting.java
+++ OrderingSystem/src/tests/OrderTesting.java
@@ -25,4 +25,18 @@ public class OrderTesting {
 		assertTrue(order.getOrderId() instanceof String);
 		assertEquals(order.getOrderId(), id);
 	}
+
+	@Test
+	public void dispatchedTrueTest(){
+		order.setDispatched(true);
+		assertTrue(order.getDispatched() instanceof Boolean);
+		assertEquals(order.getDispatched(), true);
+	}
+
+	@Test
+	public void dispatchedFalseTest(){
+		order.setDispatched(false);
+		assertEquals(order.getDispatched(), false);
+	}
+
 }
